d09b1612460d45e748263f922d33b00119ebe5a1
bernardo-campos <camposbg@gmail.com>
2026-08-31 10:04:31 -0300

    fix: navicon ya no responde json

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Models\User;
+
 test('the application returns a successful response', function () {
     $response = $this->get('/');
 
     $response->assertRedirect(route('login'));
+});
+
+test('the welcome view links authenticated users to the application', function () {
+    $this->actingAs(User::factory()->make());
+
+    $this->view('welcome')
+        ->assertSee('href="'.route('app').'"', false)
+        ->assertDontSee('href="'.url('/dashboard').'"', false);
 });

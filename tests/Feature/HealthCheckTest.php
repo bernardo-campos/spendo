5d4b1b10c7a5f2bb1b937af62a83403d095061d6
bernardo-campos <camposbg@gmail.com>
2026-09-14 13:47:18 -0300

    allow CORS on health path

--- tests/Feature/HealthCheckTest.php
+++ tests/Feature/HealthCheckTest.php
@@ -7,3 +7,19 @@ test('health check is publicly available', function () {
         ->assertSuccessful()
         ->assertExactJson(['status' => 'up']);
 });
+
+test('health check allows cross-origin requests', function () {
+    $origin = 'https://status.example.com';
+
+    $this->getJson('/health', ['Origin' => $origin])
+        ->assertSuccessful()
+        ->assertHeader('Access-Control-Allow-Origin', '*');
+
+    $this->options('/health', [], [
+        'Origin' => $origin,
+        'Access-Control-Request-Method' => 'GET',
+    ])
+        ->assertSuccessful()
+        ->assertHeader('Access-Control-Allow-Origin', '*')
+        ->assertHeader('Access-Control-Allow-Methods', 'GET');
+});
